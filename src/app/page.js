c89ac74372fbf463c14bcf1e8ef4d68b62514185
Wilber Galvez <galvezwilber22@gmail.com>
2025-02-19 10:47:48 -0400

    vista movil mejorada

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,11 @@
 import ChatCard from "../../componets/chat-card";
 
-
-
-
 export default function Home() {
-
   return (
     <div className="flex items-center justify-center h-screen w-screen bg-[url('https://wallpapercave.com/wp/wp10299398.png')] bg-cover bg-center bg-no-repeat relative">
-      <div className="w-full h-[90vh] max-w-4xl bg-white/80 backdrop-blur-md shadow-lg rounded-lg flex flex-col overflow-hidden">
-        <ChatCard />
+      <div className="w-full h-screen max-w-4xl bg-white/80 backdrop-blur-md shadow-lg rounded-lg flex flex-col overflow-hidden">
+        <ChatCard  />
       </div>
     </div>
-
   );
 }
